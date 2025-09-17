67aae468ff92257b1d0cbe85d1178f8a246c16cf
Vchuks <64593598+Vchuks@users.noreply.github.com>
2025-09-17 20:32:48 +0100

    fix: unhanled error

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const mongoose = require("mongoose")
-const winston = require("winston")
 const signupRoute = require("./routes/auth/signUp")
 const loginRoute = require("./routes/auth/login")
 const allRoutes = require("./routes/users/users")
@@ -14,33 +13,15 @@ app.use("/signup", signupRoute)
 app.use("/login", loginRoute)
 app.use("/user", allRoutes )
 
-app.use(function(err, req, res, next){
-    res.status(500).json("something went wrong")
-})
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ 
+    message: 'Something went wrong!',
+    error: process.env.NODE_ENV === 'development' ? err.message : {}
+  });
+});
 
-// const logger = winston.createLogger({
-//   level: 'info',
-//   format: winston.format.json(),
-//   defaultMeta: { service: 'user-service' },
-//   transports: [
-//     //
-//     // - Write all logs with importance level of `error` or higher to `error.log`
-//     //   (i.e., error, fatal, but not other levels)
-//     //
-//     new winston.transports.File({ filename: 'logfile.log', level: 'error' }),
-//     //
-//     // - Write all logs with importance level of `info` or higher to `combined.log`
-//     //   (i.e., fatal, error, warn, and info, but not trace)
-//     //
-//     new winston.transports.File({ filename: 'logfile.log' }),
-//   ],
-// });
-// winston.add(winston.transports.File, {filename: "logfile.log"})
-// if (process.env.NODE_ENV !== 'production') {
-//   logger.add(new winston.transports.Console({
-//     format: winston.format.simple(),
-//   }));
-// }
+
 
 if (!config.get("jwtSecretKey")){
     console.log("No jwt Key")
