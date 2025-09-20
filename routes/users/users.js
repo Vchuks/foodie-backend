ae3ba3930a94c8ef5f28c335a59bf1cc3febad45
Vchuks <64593598+Vchuks@users.noreply.github.com>
2025-09-20 15:26:08 +0100

    update

--- routes/users/users.js
+++ routes/users/users.js
@@ -3,15 +3,17 @@ const router = express.Router()
 const authM = require("../../middlewares/auth")
 const {User} = require("../../models/users")
 
+router.get("/", async(req, res) => {
+    const result = await User.find().select("fullname phonenumber")
+    res.json(result)
+})
+
 router.get("/loggedinuser", authM, async(req, res)=>{
     
     const result = await User.findById(req.eachUser.id).select("-password")
     res.json(result)
 })
 
-router.get("/", async(req, res) => {
-    const result = await User.find().select("fullname phonenumber")
-    res.json(result)
-})
+
 
 module.exports = router
